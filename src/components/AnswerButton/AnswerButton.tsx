b42205ba769e608f89863744bbcb1e2623160678
Sunghyun Cho <mail@chosunghyun.com>
2021-01-30 04:13:17 +0900

    Use AnswerButton on Evaluate Page

--- src/components/AnswerButton/AnswerButton.tsx
+++ src/components/AnswerButton/AnswerButton.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const AnswerButton = () => {
-  return <button>This is a Button</button>
+type Props = {
+  text: string
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
+
+export const AnswerButton = (props: Props) => {
+  return (
+    <AnswerButtonWrapper onClick={props.onClick}>
+      {props.text}
+    </AnswerButtonWrapper>
+  )
+}
+
+const AnswerButtonWrapper = styled.button`
+  margin: 2rem 2rem;
+`
--- src/pages/evaluate/index.tsx
+++ src/pages/evaluate/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { AnswerButton } from '../../components/AnswerButton'
 import ServiceWrapper from '../../components/ServiceWrapper'
 
 const Evaluate = () => {
+  const onClickAnswerButton = useCallback(
+    (text) => () => {
+      console.log(text)
+    },
+    []
+  )
+
   return (
     <ServiceWrapper>
-      <AnswerButton />
+      <AnswerButton
+        text={`This is a Button 1`}
+        onClick={onClickAnswerButton('val')}
+      />
+      <AnswerButton
+        text={`This is a Button 2`}
+        onClick={onClickAnswerButton('val2')}
+      />
     </ServiceWrapper>
   )
 }
