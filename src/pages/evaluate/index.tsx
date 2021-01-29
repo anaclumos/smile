17f5ce3cf91a46f6faeb71389d62ac52c12fd05c
Sunghyun Cho <mail@chosunghyun.com>
2021-01-30 04:18:47 +0900

    Add functional mapping for Buttons

--- src/pages/evaluate/index.tsx
+++ src/pages/evaluate/index.tsx
@@ -2,6 +2,29 @@ import React, { useCallback } from 'react'
 import { AnswerButton } from '../../components/AnswerButton'
 import ServiceWrapper from '../../components/ServiceWrapper'
 
+const buttons = [
+  {
+    text: 'button1text',
+    func: () => console.log(1),
+  },
+  {
+    text: 'button2text',
+    func: () => console.log(2),
+  },
+  {
+    text: 'button3text',
+    func: () => console.log(3),
+  },
+  {
+    text: 'button4text',
+    func: () => console.log(4),
+  },
+  {
+    text: 'button5text',
+    func: () => console.log(5),
+  },
+]
+
 const Evaluate = () => {
   const onClickAnswerButton = useCallback(
     (text) => () => {
@@ -12,14 +35,15 @@ const Evaluate = () => {
 
   return (
     <ServiceWrapper>
-      <AnswerButton
-        text={`This is a Button 1`}
-        onClick={onClickAnswerButton('val')}
-      />
-      <AnswerButton
-        text={`This is a Button 2`}
-        onClick={onClickAnswerButton('val2')}
-      />
+      {buttons.map((el) => {
+        return (
+          <AnswerButton
+            key={`key-${el.text}-${el.func}`}
+            text={el.text}
+            onClick={el.func}
+          />
+        )
+      })}
     </ServiceWrapper>
   )
 }
