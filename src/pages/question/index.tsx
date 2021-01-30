b306730fd5896a2b223816761ea857f2f7827006
Sunghyun Cho <mail@chosunghyun.com>
2021-01-31 03:39:58 +0900

    Add default /question page

---
+++ src/pages/question/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ServiceWrapper from '../../components/ServiceWrapper'
+
+const index = () => {
+  return <ServiceWrapper>Question</ServiceWrapper>
+}
+
+export default index
