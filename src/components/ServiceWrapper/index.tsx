0b43b939c4e3fe0617f8d46589ff2482fbefdded
Sunghyun Cho <mail@chosunghyun.com>
2021-01-31 03:41:23 +0900

    Expand ServiceWrapper to have Title Tag

--- src/components/ServiceWrapper/index.tsx
+++ src/components/ServiceWrapper/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import GlobalNavigationBar from './GlobalNavigationBar'
+import Head from 'next/head'
 
-const ServiceWrapper: React.FC = ({ children }) => {
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+const ServiceWrapper = ({
+  children,
+  title = `sunghyun's smile system`,
+}: Props) => {
   return (
     <Layout>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <GlobalNavigationBar />
       <ContentWrapper>{children}</ContentWrapper>
     </Layout>
