57f766ddd2b137719b2932fcf57a676c10ce9046
Sunghyun Cho <mail@chosunghyun.com>
2021-01-31 06:47:37 +0900

    Separate Question and Video Data

---
+++ src/data/videos.ts
@@ -0,0 +1,31 @@
+// type Video
+// id: number
+// title: string
+// uploader: string
+// desc: string
+// youtubeId: string
+
+export const videoData = [
+  {
+    id: 1,
+    title: '나가지 말라면 나가지 마',
+    uploader: '위즈원아님',
+    desc:
+      'So you like IZ*ONE? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac porta justo, non laoreet nibh. Curabitur purus ligula, egestas et ultrices in, laoreet in sapien. ',
+    youtubeId: 'RQbNZhDey3c',
+  },
+  {
+    id: 2,
+    title: 'How You Like Y.M.C.A.',
+    uploader: 'J.E.B',
+    desc: 'So you like BlackPink?',
+    youtubeId: 'NY3iWMyp8y8',
+  },
+  {
+    id: 3,
+    title: 'how we write/review code in big tech companies',
+    uploader: 'Joma Tech',
+    desc: 'So you like coding?',
+    youtubeId: 'rR4n-0KYeKQ',
+  },
+]
