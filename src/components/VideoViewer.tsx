47d219f583961c3d1570fcceac1d8f845d2c0f08
Sunghyun Cho <mail@chosunghyun.com>
2021-01-31 04:00:03 +0900

    Style VideoViewer

--- src/components/VideoViewer.tsx
+++ src/components/VideoViewer.tsx
@@ -9,20 +9,43 @@ type Props = {
 const VideoViewer = ({ video: video }: Props) => {
   return (
     <div>
-      <QuestionHeader>{video.title}.</QuestionHeader>
-      {video.desc}
-      <iframe
-        width="768"
-        height="432"
-        src={`https://www.youtube.com/embed/${video.youtubeId}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <VideoHeader>{video.title}</VideoHeader>
+      <VideoDesc>{video.desc}</VideoDesc>
+      <YouTubeIframeWrapper>
+        <YouTubeIframe
+          src={`https://www.youtube.com/embed/${video.youtubeId}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </YouTubeIframeWrapper>
     </div>
   )
 }
 
-const QuestionHeader = styled.h1``
+const VideoHeader = styled.h1`
+  line-break: keep-all;
+  line-height: 1.6;
+`
 
+const YouTubeIframeWrapper = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+  padding-top: 56.25%;
+  position: relative;
+`
+const YouTubeIframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
+
+const VideoDesc = styled.p`
+  line-break: keep-all;
+  line-height: 1.6;
+`
 export default VideoViewer
